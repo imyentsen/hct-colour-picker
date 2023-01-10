4dc9e6feac25c4c528eb8b2f605624d135fd5f5f
Ansin Liu <ansinliu@Ansins-MacBook-Pro.local>
2023-01-10 09:56:42 +0100

    first commit

---
+++ code.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var material_color_utilities_1 = require("@material/material-color-utilities");
+// This plugin will open a window to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = function (msg) {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    if (msg.type === 'create-color') {
+        // Get the number of selected elements
+        var selectedElements = figma.currentPage.selection.length;
+        // Display error messages on invalid sleection
+        if (selectedElements === 0) {
+            figma.closePlugin('No element selected!');
+            return;
+        }
+        if (selectedElements > 1) {
+            figma.closePlugin('Please select a single element!');
+            return;
+        }
+        // Fill the current selection
+        var node = figma.currentPage.selection[0];
+        if (node.type === 'RECTANGLE') {
+            // make sure the HCT input is in correct format
+            if (msg.huevalue || msg.chromavalue || msg.tonevalue === undefined || msg.huevalue.length || msg.chromavalue.length || msg.tonevalue.length === 0) {
+                figma.closePlugin('HCT values are undefined');
+            }
+            if (msg.huevalue.some(isNaN) || msg.chromavalue.some(isNaN) || msg.tonevalue.some(isNaN)) {
+                figma.closePlugin('HCT values are not numbers');
+            }
+            var color = material_color_utilities_1.Hct.from(Number(msg.huevalue), Number(msg.chromavalue), Number(msg.tonevalue));
+            var hex = (0, material_color_utilities_1.hexFromArgb)(color.toInt());
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            var r = 0;
+            var g = 0;
+            var b = 0;
+            if (result) {
+                var color_1 = {
+                    r: parseInt(result[1], 16),
+                    g: parseInt(result[2], 16),
+                    b: parseInt(result[3], 16),
+                };
+                r = color_1.r;
+                g = color_1.g;
+                b = color_1.b;
+            }
+            else {
+                figma.closePlugin('Invalid color string');
+            }
+            console.log(hex);
+            var rect = node;
+            rect.fills = [{ type: 'SOLID', color: { r: r, g: g, b: b } }];
+        }
+    }
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
+};
